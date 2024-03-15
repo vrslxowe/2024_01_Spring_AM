58e650e48357ce9d2ff2dc5545e33e953373d945
vrslxowe <vrslxowe@gmail.com>
2024-03-15 17:50:28 +0900

    test project-11

---
+++ src/main/java/com/example/demo/vo/DiaryMember.java
@@ -0,0 +1,25 @@
+package com.example.demo.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class DiaryMember {
+	private int id;
+	private String regDate;
+	private String updateDate;
+	private String loginId;
+	private String loginPw;
+	private int authLevel;
+	private String name;
+	private String nickname;
+	private String birth_date;
+	private String phone_number;
+	private String email;
+	private boolean delStatus;
+	private String delDate;
+	
+}
